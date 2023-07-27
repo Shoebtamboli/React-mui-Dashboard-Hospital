e6f2adfc7ced4ff7a0afbcce0934669be734951d
shoeb <shoebtamboli98@gmail.com>
2023-07-27 23:05:21 +0200

    fixed loader

--- src/components/HeartRateLoader.tsx
+++ src/components/HeartRateLoader.tsx
@@ -1,42 +1,38 @@
-//import { CircularProgress } from "@mui/material";
-import { makeStyles } from "@mui/styles";
-import Diversity1OutlinedIcon from "@mui/icons-material/Diversity1Outlined";
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
-    height: "100vh",
-    "& svg": {
-      animation: "$heartbeat 1s ease-in-out infinite"
-    }
-  },
-  "@keyframes heartbeat": {
-    "0%": {
-      transform: "scale(1)"
-    },
-    "50%": {
-      transform: "scale(1.3)"
-    },
-    "100%": {
-      transform: "scale(1)"
-    }
-  }
-}));
+import { Diversity1Outlined as Diversity1OutlinedIcon } from "@mui/icons-material";
+import Box from "@mui/material/Box";
 
 const HeartRateLoader = ({ message }: any) => {
-  const classes = useStyles();
-
   return (
-    <div className={classes.root}>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        "& svg": {
+          animation: "heartbeat 1s ease-in-out infinite",
+        },
+        "@keyframes heartbeat": {
+          // Define the keyframes here
+          "0%": {
+            transform: "scale(1)",
+          },
+          "50%": {
+            transform: "scale(1.3)",
+          },
+          "100%": {
+            transform: "scale(1)",
+          },
+        },
+      }}
+    >
       <Diversity1OutlinedIcon
         color="primary"
         sx={{ fontSize: "6rem", mb: 2 }}
       />
       {message && <p style={{ marginTop: 20, fontSize: 18 }}>{message}</p>}
-    </div>
+    </Box>
   );
 };
 
